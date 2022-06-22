6108691d8fb7b4c9d11a5ad67e8aa0ee5da6b975
aa.petrushin <andrey.petrushin90@gmail.com>
2022-06-22 18:08:59 +0300

    Complete lab1.3

--- Lab1.3/SNMP_Lab.py
+++ Lab1.3/SNMP_Lab.py
@@ -4,6 +4,19 @@ result = getCmd(SnmpEngine(),
                 CommunityData("public", mpModel=0),
                 UdpTransportTarget(("10.31.70.107", 161)),
                 ContextData(), ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)))
+
+result2 = nextCmd(SnmpEngine(),
+                CommunityData("public", mpModel=0),
+                UdpTransportTarget(("10.31.70.107", 161)),
+                ContextData(),
+                ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.2')),
+                lexicographicMode=False)
+
 for i in result:
+    for s in i[3]:
+        print(s)
+print('')
+
+for i in result2:
     for s in i[3]:
         print(s)
