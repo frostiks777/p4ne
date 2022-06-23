f64dd7b0040bab961feae0b241f32d6857616ee6
aa.petrushin <andrey.petrushin90@gmail.com>
2022-06-23 18:19:41 +0300

    Add ipplan.py without sorted

---
+++ Lab1.4/ipplan.py
@@ -0,0 +1,17 @@
+import ipaddress
+import random
+
+class IPv4RandomNetwork(ipaddress.IPv4Network):
+    def __init__(self):
+        ipaddress.IPv4Network.__init__(self, (random.randrange(0x0B000000, 0xDF000000), random.randrange(8, 24)), strict=False)
+    def regular(self):
+        return not self.is_private and not self.is_multicast
+    def key_value(self):
+        net = self.network_address
+        mask = self.netmask
+        return int(mask)*2**32+int(net)
+
+for i in range(5):
+    net1 = IPv4RandomNetwork()
+    print(net1)
+    print(net1.key_value())
