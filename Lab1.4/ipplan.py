767175a0a143189e4a6882df20e64fcb239fb28d
aa.petrushin <andrey.petrushin90@gmail.com>
2022-06-24 09:46:40 +0300

    Complete Lab1.4

--- Lab1.4/ipplan.py
+++ Lab1.4/ipplan.py
@@ -3,7 +3,9 @@ import random
 
 class IPv4RandomNetwork(ipaddress.IPv4Network):
     def __init__(self):
-        ipaddress.IPv4Network.__init__(self, (random.randrange(0x0B000000, 0xDF000000), random.randrange(8, 24)), strict=False)
+        ipaddress.IPv4Network.__init__(self, (random.randrange(0x0B000000, 0xDF000000),
+                                              random.randrange(8, 24)),
+                                       strict=False)
     def regular(self):
         return not self.is_private and not self.is_multicast
     def key_value(self):
@@ -11,7 +13,13 @@ class IPv4RandomNetwork(ipaddress.IPv4Network):
         mask = self.netmask
         return int(mask)*2**32+int(net)
 
+d = {}
 for i in range(5):
     net1 = IPv4RandomNetwork()
-    print(net1)
-    print(net1.key_value())
+    while not net1.regular():
+        net1 = IPv4RandomNetwork()
+    d.update({net1.key_value():net1.exploded})
+d = dict(sorted(d.items(), key=lambda item: item[1]))
+l = list(d.values())
+for a in l:
+    print(a)
